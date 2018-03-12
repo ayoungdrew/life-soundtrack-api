a10c2dcdf87eb26301b1a47cded9368577d51f99
ayoungdrew <andrewyoung504@gmail.com>
2018-03-12 17:52:33 -0400

    Attempting to rearrange old migration files
    Attempting to rearrange old migration files

--- db/migrate/20180307164153_remove_story_from_songs.rb
+++ db/migrate/20180307164153_remove_story_from_songs.rb
@@ -1,5 +1,11 @@
 class RemoveStoryFromSongs < ActiveRecord::Migration[5.1]
   def change
     # remove_column :songs, :story, :text
+    create_table :songs do |t|
+      t.string :name
+      t.string :artist
+
+      t.timestamps
+    end
   end
 end
